d060cd5ea01e19ad6bbc14d9a1104c15c4bc922d
Alex Ponebshek <alexbobp@gmail.com>
2017-08-02 02:04:26 -0500

    return of rez sickness saturation boost

--- mod/src/main/java/com/elytradev/fondue/module/rezsickness/PotionResurrectionSickness.java
+++ mod/src/main/java/com/elytradev/fondue/module/rezsickness/PotionResurrectionSickness.java
@@ -1,5 +1,8 @@
 package com.elytradev.fondue.module.rezsickness;
 
+import com.elytradev.concrete.reflect.accessor.Accessor;
+import com.elytradev.concrete.reflect.accessor.Accessors;
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.GlStateManager;
@@ -23,16 +26,18 @@ public class PotionResurrectionSickness extends Potion {
 	public boolean isReady(int duration, int amplifier) {
 		return true;
 	}
-	
-//	@Override TODO
-//	public void performEffect(EntityLivingBase elb, int amp) {
-//		if (elb instanceof EntityPlayer) {
-//			// death isn't a food source, just a sneaky saturation source
-//			FoodStats foods = ((EntityPlayer)elb).getFoodStats();
-//			float sat = foods.getSaturationLevel();
-//			if (sat >= 1 && sat < 12) foods.setFoodSaturationLevel(sat + 0.03f);
-//		}
-//	}
+
+	private final Accessor<Float> saturationLevel = Accessors.findField(FoodStats.class, "foodSaturationLevel", "field_75125_b", "b");
+
+	@Override
+	public void performEffect(EntityLivingBase elb, int amp) {
+		if (elb instanceof EntityPlayer) {
+			// death isn't a food source, just a sneaky saturation source
+			FoodStats foods = ((EntityPlayer)elb).getFoodStats();
+			float sat = foods.getSaturationLevel();
+			if (sat >= 1 && sat < 12) saturationLevel.set(foods, sat + 0.03f);
+		}
+	}
 	
 	@Override
 	public void renderHUDEffect(int x, int y, PotionEffect effect, Minecraft mc, float alpha) {
